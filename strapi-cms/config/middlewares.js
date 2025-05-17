af4411592aefd685b317b91846072b286adccb97
Utak-West <utak.west@gmail.com>
2025-05-17 17:53:13 -0400

    feat: initialize Strapi CMS with content types for artists, artworks, exhibits and wellness services

---
+++ strapi-cms/config/middlewares.js
@@ -0,0 +1,28 @@
+module.exports = [
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+      frameguard: {
+        action: 'sameorigin',
+      },
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
